2a5b92a180de20210c471b47e2bd341eb86abb39
Cejudo <cejudo@outlook.com>
2020-07-13 00:39:06 -0500

    Init c# console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageToANSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
